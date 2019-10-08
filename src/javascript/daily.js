a1f7e717453ae2779ce15a503c557219f99e4c24
stackker <sunil.suniljoseph@gmail.com>
2019-10-08 15:26:52 -0600

    testing revised 08/10

---
+++ src/javascript/daily.js
@@ -0,0 +1,17 @@
+
+const functions = {
+    
+    size: () => {
+        /* if (num < 0) return "Negative";
+        if (num < 10) return "small";
+        if (num < 20) return "medium"; */
+
+
+        return  ("Hello World");
+    },
+
+
+    
+};
+
+export default functions;
---
+++ src/javascript/daily.test.js
@@ -0,0 +1,8 @@
+import functions from './daily'
+
+test('Check the sizes', () => {
+
+ expect(functions.size()).toBe ("Hello World");  // Consider the edge cases
+  // console.log("Hello World from test")
+});
+
