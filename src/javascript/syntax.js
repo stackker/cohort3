245084a60a82c3a3fece88f214f95c0997e14ec1
stackker <sunil.suniljoseph@gmail.com>
2019-10-08 16:45:15 -0600

    Initializing Syntax Exercise

---
+++ src/javascript/syntax.js
@@ -0,0 +1,12 @@
+// define attributes / variables
+// number
+// string
+// boolean
+// array
+// dictionary / objects
+// undefined
+// sample if / else
+// functions
+// parameters
+// returns
+// arrays
