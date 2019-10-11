ee1bb6e9a61fed3c47176dcaeed1defaa94bd2e7
stackker <sunil.suniljoseph@gmail.com>
2019-10-11 09:46:30 -0600

    makeEmailAr Completedt

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -10,8 +10,26 @@ const functions = {
         return  ("Hello World");
     },
 
+    makeEmailArr : (name) => {
+        if(name instanceof Array===true) {
+            let nameString =  name.map(changeCase);
+            let emailName =nameString.join(".");
+
+            let emailAdd = `${emailName}@evolveu.ca`;
+            return emailAdd;
+        }
+        else {
+            console.log("Pass Name as an Array");
+        }
+
+
+    }
+
 
     
 };
+function changeCase(text) {
+    return text.toLowerCase();
+}
 
 export default functions;
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -1,8 +1,18 @@
 import functions from './daily'
-
+/* 
 test('Check the sizes', () => {
 
  expect(functions.size()).toBe ("Hello World");  // Consider the edge cases
   // console.log("Hello World from test")
 });
+ */
 
+test('email builder from an array', () => {
+  const name = ["first", "last"];
+  expect(functions.makeEmailArr(name))
+      .toEqual("first.last@evolveu.ca");
+  expect(functions.makeEmailArr(["First", "Last"]))
+      .toEqual("first.last@evolveu.ca");
+  expect(functions.makeEmailArr(["Bill", "Smith"]))
+      .toEqual("bill.smith@evolveu.ca");
+});
