40fe287f6c31ef9469ba713a2609f69b47d44c83
stackker <sunil.suniljoseph@gmail.com>
2019-10-16 18:45:36 -0600

    Prepare for Array Work - October 15, 2019

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,13 +1,70 @@
 
 const functions = {
 
+
+    // Prepare for Array Work - October 15, 2019
+
+    forloop: (count) => {
+        let i;
+        let output = [];
+        for (i = 0; i < count; i++) {
+            output.push(i);
+        }
+        return output;
+    },
+
+    whileloop: (condn) => {
+        while (condn !== true) {
+            return `While input is ${condn}`;
+        }
+
+    },
+
+    dowhile: (count) => {
+        let i = 0;
+        let output = [];
+        do {
+            output.push(i++);
+        } while (i < count);
+        return output;
+
+    },
+
+    forin: (bio) => {
+
+        var stringobj = "";
+        
+        for (var char in bio) {
+            stringobj += bio[char];
+        }
+        return stringobj;
+
+    },
+
+    forof: (arr) => {
+        let arr1 =[];
+        let output =[];
+        for (const entry of arr ) {
+           // console.log("entry: ",entry)
+            arr1[0] = entry;
+            output.push([arr1[0]]);
+        }
+        return output;
+
+    },
+
+
+
+
     size: () => {
         /* if (num < 0) return "Negative";
         if (num < 10) return "small";
-        if (num < 20) return "medium"; */
+        if (num < 20) return "medium";
+        else
+            return ("Hello World"); */
 
 
-        return ("Hello World");
+
     },
 
     makeEmailArr: (name) => {
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -1,5 +1,37 @@
 import functions from './daily'
-/* 
+// for
+test('demo for', () => {
+    let fortimes = 3;
+    expect(functions.forloop(fortimes))
+        .toEqual([0, 1, 2]);
+    expect(functions.forloop(7))
+        .toEqual([0, 1, 2, 3, 4, 5, 6]);
+    // while
+    expect(functions.whileloop(true)).toEqual();
+    expect(functions.whileloop(false)).toEqual("While input is false");
+
+    // do while
+    expect(functions.dowhile(5)).toEqual([0,1,2,3,4]);
+
+    // for in 
+    expect(functions.forin({name:"sid",age: 21,height: "155 cm"})).toEqual("sid21155 cm");
+
+    // for of
+    expect(functions.forof ([1,2,3,4])).toEqual([[1],[2],[3],[4]]);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 test('Check the sizes', () => {
 
  expect(functions.size()).toBe ("Hello World");  // Consider the edge cases
@@ -17,14 +49,14 @@ test('Check the sizes', () => {
       .toEqual("bill.smith@evolveu.ca");
 }); */
 
-/*	
+/*
 	Write the function to format an email based on an object / map
 */
 
 
 // tests for makeEmailObj - Oct 11, 2019
 
-test('email builder from an object / map', () => {
+/* test('email builder from an object / map', () => {
     const name = { fname: 'first', lname: 'last' };
     expect(functions.makeEmailObj(name))
         .toEqual("first.last@evolveu.ca");
@@ -32,4 +64,4 @@ test('email builder from an object / map', () => {
         .toEqual("first.last@evolveu.ca");
     expect(functions.makeEmailObj({ fname: "Bill", lname: "Smith" }))
         .toEqual("bill.smith@evolveu.ca");
-});
+}); */
