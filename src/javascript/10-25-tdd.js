672dbf087381371628b745d3930e25d4db1b2add
stackker <sunil.suniljoseph@gmail.com>
2019-10-25 13:34:26 -0600

    sample for creting the TDD

---
+++ src/javascript/10-25-tdd.js
@@ -0,0 +1,16 @@
+const functions = {
+
+    helloWorld: () =>{
+
+    },
+// A STUB has a function with a name, parameters and returns a dummy
+    sum:(a, b) =>{
+
+
+    return "Dummy";
+    }
+}
+
+
+export default functions;
+
---
+++ src/javascript/10-25-tdd.test.js
@@ -0,0 +1,22 @@
+
+//0. Write the test without expect  with a console log to ensure that the test is picked up
+//1. Write theStub first with a dummy return
+//2. Write a failing test
+//3. Write min code to pass the test
+//4. Repeat with a return to fail
+//5. Modify the function to make it pass
+//6. 
+
+
+import functions from "./10-25-tdd.js"
+
+
+test('test helloWorld', () => {
+
+    console.log("test helloWorld")
+});
+
+test('sumd', () => {
+
+    console.log("test sum")
+});
