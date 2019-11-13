f808a7deeec2852cd0894148bc453430266104f4
stackker <sunil.suniljoseph@gmail.com>
2019-11-12 19:59:25 -0700

    Account &Controller methods tested

--- src/03-objects/accounts.js
+++ src/03-objects/accounts.js
@@ -1,43 +1,106 @@
 
 class Accounts {
-    constructor(accName, bal) {
-        this.accName = accName;
-        this.bal = bal;
-    }
+  constructor(accName, bal) {
+    this.accName = accName;
+    this.bal = bal;
+  }
 
-    deposit(depAmt) {
+  deposit(depAmt) {
 
-        if (depAmt <= 0) {
-            let error = "Deposit Amt must be greater than 0";
-            console.log(error);
-            return error;
-        } else {
-            this.bal += depAmt;
-            return this.bal;
-        }
+    if (depAmt <= 0) {
+      let error = "Deposit Amt must be greater than 0";
+      console.log(error);
+      return error;
+    } else {
+      this.bal += depAmt;
+      return this.bal;
     }
+  }
 
-    withdraw(withdrawAmt) {
+  withdraw(withdrawAmt) {
 
-        if (withdrawAmt<= 0) {
-            let error = "Withdrawn Amt must be greater than 0";
-            console.log(error);
-            return error;
-        } else {
-            this.bal -= withdrawAmt;
-            return this.bal
-        }
+    if (withdrawAmt <= 0) {
+      let error = "Withdrawn Amt must be greater than 0";
+      console.log(error);
+      return error;
+    } else {
+      this.bal -= withdrawAmt;
+      return this.bal
     }
-    balance() {
-        return this.bal;
-    }
+  }
+  getbalance() {
+    return this.bal;
+  }
 }
+
+class AccountController {
+
+  constructor() {
+    this.accountList = [];
+
+  }
+
+  createAccount(accName, bal) {
+    // console.log("accName: ", accName, "bal: ", bal);
+    this.account = new Accounts(accName, bal);
+    // let account.accName = accName;
+    // let account.bal = bal;
+    this.accountList.push(this.account);
+  }
+
+  removeAccount(accName) {
+    this.accountList = this.accountList.filter(account => {
+      return account.accName !== accName
+    });
+  }
+
+  totalAccounts() {
+    // console.log("accountList :", this.accountList)
+    return this.accountList.reduce(((acc, account) => acc + account.bal), 0);
+  }
+
+  highestAccount() {
+    let maxAccount;
+    let i = 0;
+    let maxVal = this.accountList[0].bal;
+    this.accountList.forEach((account, indx) => {
+      //console.log(account.bal, indx);
+      if (maxVal <= account.bal) {
+        maxVal = account.bal;
+        maxAccount = account;
+      }
+      // console.log(maxVal, account.bal, indx, maxAccount);
+    });
+    return maxAccount;
+  }
+
+
+  lowestAccount() {
+    let minAccount;
+    let i = 0;
+    let minVal = this.accountList[0].bal;
+    let lowAccount = this.accountList.forEach((account, indx) => {
+      //console.log(account.bal, indx);
+      if (minVal >= account.bal) {
+        minVal = account.bal;
+        minAccount = account;
+      }
+      // console.log(minVal, account.bal, indx, minAccount);
+    });
+    return minAccount;
+  }
+
+}
+
+
+
+
 
 
 const functions = {
-    // nothing in here as of yet
+  // nothing in here as of yet
 }
-export { Accounts, functions };
+export { Accounts, AccountController, functions };
 
 
 
--- src/03-objects/accounts.test.js
+++ src/03-objects/accounts.test.js
@@ -3,7 +3,7 @@ describe("Testing Account properties and methods", () => {
     const Acct = new oo.Accounts("checkingAccount", 25)
 
     test("check testing", () => {
-        // console.log("From 03-objects-functions.test.js");
+        console.log("From 03-objects-functions.test.js");
     });
 
     test("check Paramenter Passing", () => {
@@ -13,19 +13,71 @@ describe("Testing Account properties and methods", () => {
 
     });
 
-    test("check method balance", () => {
+    test("check method bal", () => {
         //console.log(Acct)
-        expect(Acct.balance()).toBe(25);
+        expect(Acct.getbalance()).toBe(25);
     });
     test("check method deposit", () => {
-        console.log(Acct)
+        //console.log(Acct)
         expect(Acct.deposit(10)).toBe(35);
         expect(Acct.deposit(-20)).toBe("Deposit Amt must be greater than 0")
     });
 
     test("check method withdrawal", () => {
-        console.log(Acct)
+        //console.log(Acct)
         expect(Acct.withdraw(30)).toBe(5);
         expect(Acct.withdraw(-20)).toBe("Withdrawn Amt must be greater than 0");
     });
 })
+describe('Account Controller Tests', () => {
+    let accountManager = new oo.AccountController();
+
+    test('Check Account creation', () => {
+        accountManager.createAccount("Savings Account", 100)
+        expect(accountManager.accountList[0].accName).toEqual("Savings Account");
+        expect(accountManager.accountList[0].bal).toEqual(100)
+        accountManager.createAccount("Checking Account", 200);
+        expect(accountManager.accountList[1]).toEqual({ accName: "Checking Account", bal: 200 })
+        //   console.log(accountManager.accountList)
+
+    });
+
+
+    test('Check Account removal', () => {
+        //   console.log(accountManager.accountList)
+        accountManager.removeAccount("Savings Account")
+        expect(accountManager.accountList[0]).toEqual({ "accName": "Checking Account", "bal": 200 });
+        expect(accountManager.accountList).toEqual([{ "accName": "Checking Account", "bal": 200 }]);
+        // console.log(accountManager.accountList)
+        accountManager.removeAccount("Checking Account");
+        expect(accountManager.accountList).toEqual([]);
+    });
+
+    test('Check Account Total', () => {
+
+        accountManager.createAccount("Savings Account", 2000);
+        accountManager.createAccount("Checking Account", 1000);
+        expect(accountManager.totalAccounts()).toEqual(3000);
+    });
+
+    test('Check Account with Highest bal', () => {
+        let accountManager = new oo.AccountController();
+        accountManager.createAccount("Checking Account1", 10);
+        accountManager.createAccount("Checking Account", 1000);
+        accountManager.createAccount("Savings Account1", 200);
+        // console.log("Begin Highest: ", accountManager.accountList)
+        expect(accountManager.highestAccount()).toEqual({ accName: 'Checking Account', bal: 1000 });
+    });
+        
+
+    test('Check Account with Highest bal', () => {
+        let accountManager = new oo.AccountController();
+        accountManager.createAccount("Checking Account1", 10);
+        accountManager.createAccount("Checking Account", 1000);
+        accountManager.createAccount("Savings Account1", 200);
+        // console.log("Begin Lowest ", accountManager.accountList)
+        expect(accountManager.lowestAccount()).toEqual({ "accName": "Checking Account1", "bal": 10 });
+    });
+
+});
+
