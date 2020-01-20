2ec5a9e7e0a655188ed1057baf58494b2f49a666
stackker <sunil.suniljoseph@gmail.com>
2020-01-20 14:05:03 -0700

    miscellaneous changes

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-
 import "./App.css";
 import Navbar from "./Components/Navbar.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   render() {
