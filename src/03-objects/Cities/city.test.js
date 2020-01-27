c6aff89ae3cada7cfc580b8304ed44d290f27e48
stackker <sunil.suniljoseph@gmail.com>
2020-01-26 23:17:59 -0700

    prettier

--- src/03-objects/Cities/city.test.js
+++ src/03-objects/Cities/city.test.js
@@ -1,142 +1,130 @@
 import { City, Community } from "./city.js";
 
-
 test("check test", () => {
-	console.log("testing from city.test.js");
+  console.log("testing from city.test.js");
 });
 test("check Show Method", () => {
-
-	let TestCity = new City(1, "MyCity", 21.245, -55.250, 99);
-	expect(TestCity.show()).toEqual("MyCity, 21.245, -55.25, 99");
-
+  let TestCity = new City(1, "MyCity", 21.245, -55.25, 99);
+  expect(TestCity.show()).toEqual("MyCity,21.245,-55.25,99");
 });
 
 test("check howBig Method", () => {
-	let TestCity = new City(1, "MyCity", 21.245, -55.250, 99);
-	expect(TestCity.howBig()).toBe("Hamlet");
+  let TestCity = new City(1, "MyCity", 99, 21.245, -55.25);
+  expect(TestCity.howBig()).toBe("Hamlet");
 
-	TestCity = new City(2, "MyCity", 21.245, -55.250, 999);
-	expect(TestCity.howBig()).toBe("Village");
+  TestCity = new City(2, "MyCity", 999, 21.245, -55.25);
+  expect(TestCity.howBig()).toBe("Village");
 
-	TestCity = new City(3, "MyCity", 21.245, -55.250, 9999);
-	expect(TestCity.howBig()).toBe("Town");
+  TestCity = new City(3, "MyCity", 9999, 21.245, -55.25);
+  expect(TestCity.howBig()).toBe("Town");
 
-	TestCity = new City(4, "MyCity", 21.245, -55.250, 99999);
-	expect(TestCity.howBig()).toBe("Large Town");
+  TestCity = new City(4, "MyCity", 99999, 21.245, -55.25);
+  expect(TestCity.howBig()).toBe("Large Town");
 
-	TestCity = new City(5, "MyCity", 21.245, -55.250, 999999);
-	expect(TestCity.howBig()).toBe("City");
-
+  TestCity = new City(5, "MyCity", 999999, 21.245, -55.25);
+  expect(TestCity.howBig()).toBe("City");
 });
 
 test("check MovedIn Method", () => {
-	let TestCity = new City(1, "MyCity", 21.245, -55.250, 99);
-	expect(TestCity.howBig()).toBe("Hamlet");
-	expect(TestCity.movedin(10)).toBe(109);
-	expect(TestCity.howBig()).toBe("Village");
-
-
+  let TestCity = new City(1, "MyCity", 99, 21.245, -55.25);
+  expect(TestCity.howBig()).toBe("Hamlet");
+  expect(TestCity.movedin(10)).toBe(109);
+  expect(TestCity.howBig()).toBe("Village");
 });
 
 test("check MovedOut Method", () => {
-	let TestCity = new City(1, "MyCity", 21.245, -55.250, 100);
+  let TestCity = new City(1, "MyCity", 100, 21.245, -55.25);
 
-	expect(TestCity.movedOut(10)).toBe(90);
-	expect(TestCity.movedOut(200)).toBe(90);
-	expect(TestCity.movedOut(-10)).toBe(90);
-	TestCity.movedin(10010);
-	expect(TestCity.movedOut(10)).toBe(10090);
-
+  expect(TestCity.movedOut(10)).toBe(90);
+  expect(TestCity.movedOut(200)).toBe(90);
+  expect(TestCity.movedOut(-10)).toBe(90);
+  TestCity.movedin(10010);
+  expect(TestCity.movedOut(10)).toBe(10090);
 });
 
 test("Community:  createCity Method", () => {
-	let MetroData = new Community;
-	// let TestCity =  new City("MyCity", 21.245, -55.250, 100);
-	// expect(MetroData.createCity("MyVillage", 21.245, -55.250, 100)).toEqual(["MyVillage", 21.245, -55.250, 100]);
-	MetroData.createCity(1, "MyVillage", 21.245, -55.250, 100);
-	expect(MetroData.createCity(2, "MyTown", 121.245, -65.250, 1020)).toEqual
-		([
-			{
-				"key": 1,
-				"city": "MyVillage",
-				"lat": 21.245,
-				"long": -55.25,
-				"population": 100
-			},
-			{
-				"key": 2,
-				"city": "MyTown",
-				"lat": 121.245,
-				"long": -65.25,
-				"population": 1020
-			}
-		]);
+  let MetroData = new Community();
+  // let TestCity =  new City("MyCity", 21.245, -55.250, 100);
+  // expect(MetroData.createCity("MyVillage", 21.245, -55.250, 100)).toEqual(["MyVillage", 21.245, -55.250, 100]);
+  MetroData.createCity(1, "MyVillage", 100, 21.245, -55.25);
+  expect(MetroData.createCity(2, "MyTown", 1020, 121.245, -65.25)).toEqual([
+    {
+      key: 1,
+      city: "MyVillage",
+      lat: 21.245,
+      long: -55.25,
+      population: 100
+    },
+    {
+      key: 2,
+      city: "MyTown",
+      lat: 121.245,
+      long: -65.25,
+      population: 1020
+    }
+  ]);
 });
 test("Community: deleteCity Method", () => {
-	let MetroData = new Community;
+  let MetroData = new Community();
 
-	MetroData.createCity(1, "MyVillage", 21.245, -55.250, 100);
-	MetroData.createCity(2, "MyTown", 121.245, -65.250, 1020);
-	MetroData.createCity(3, "MyCity", 121.245, -25.250, 10200);
-	MetroData.deleteCity("MyTown");
-	expect(MetroData.deleteCity("MyTown")).toEqual([
-		{
-			key: 1,
-			city: 'MyVillage',
-			lat: 21.245,
-			long: -55.25,
-			population: 100
-		},
-		{
-			key: 3,
-			city: 'MyCity',
-			lat: 121.245,
-			long: -25.25,
-			population: 10200
-		}]);
-
+  MetroData.createCity(1, "MyVillage", 100, 21.245, -55.25);
+  MetroData.createCity(2, "MyTown", 1020, 121.245, -65.25);
+  MetroData.createCity(3, "MyCity", 10200, 121.245, -25.25);
+  MetroData.deleteCity("MyTown");
+  expect(MetroData.deleteCity("MyTown")).toEqual([
+    {
+      key: 1,
+      city: "MyVillage",
+      lat: 21.245,
+      long: -55.25,
+      population: 100
+    },
+    {
+      key: 3,
+      city: "MyCity",
+      lat: 121.245,
+      long: -25.25,
+      population: 10200
+    }
+  ]);
 });
 
 test("check getPopulation Method", () => {
-	let MetroData = new Community;
+  let MetroData = new Community();
 
-	MetroData.createCity(1, "MyVillage", 21.245, -55.250, 100);
-	MetroData.createCity(2, "MyTown", 121.245, -65.250, 1000);
-	MetroData.createCity(3, "MyCity", 121.245, -25.250, 10000);
-	// console.log(MetroData.getPopulation());
-	expect(MetroData.getPopulation()).toEqual(11100);
+  MetroData.createCity(1, "MyVillage", 100, 21.245, -55.25);
+  MetroData.createCity(2, "MyTown", 1000, 121.245, -65.25);
+  MetroData.createCity(3, "MyCity", 10000, 121.245, -25.25);
+  // console.log(MetroData.getPopulation());
+  expect(MetroData.getPopulation()).toEqual(11100);
 });
 
 test("check getMostSouthern() method", () => {
-	let MetroData = new Community;
+  let MetroData = new Community();
 
-	MetroData.createCity(1, "MyVillage", 21.245, -55.250, 100);
-	MetroData.createCity(2, "MyTown", 121.245, -65.250, 1000);
-	MetroData.createCity(3, "MyCity", 121.245, -25.250, 10000);
-	MetroData.createCity(4, "NewYork", 80.245, 25.250, 100000);
-	MetroData.createCity(5, "Inuktituk", 80.245, 68.250, 100000);
-	expect(MetroData.getMostSouthern().city).toEqual("MyTown");
-})
+  MetroData.createCity(1, "MyVillage", 100, 21.245, -55.25);
+  MetroData.createCity(2, "MyTown", 1000, 121.245, -65.25);
+  MetroData.createCity(3, "MyCity", 10000, 121.245, -25.25);
+  MetroData.createCity(4, "NewYork", 100000, 80.245, 25.25);
+  MetroData.createCity(5, "Inuktituk", 100000, 80.245, 68.25);
+  expect(MetroData.getMostSouthern().city).toEqual("MyTown");
+});
 
 test("check getMostNorthern() method", () => {
-	let MetroData = new Community;
+  let MetroData = new Community();
 
-	MetroData.createCity(1, "MyVillage", 21.245, -55.250, 100);
-	MetroData.createCity(2, "MyTown", 121.245, -65.250, 1000);
-	MetroData.createCity(3, "MyCity", 121.245, -25.250, 10000);
-	MetroData.createCity(4, "NewYork", 80.245, 25.250, 100000);
-	MetroData.createCity(5, "Inuktituk", 80.245, 68.250, 100000);
-	expect(MetroData.getMostNorthern().city).toEqual("Inuktituk");
-})
-
+  MetroData.createCity(1, "MyVillage", 100, 21.245, -55.25);
+  MetroData.createCity(2, "MyTown", 1000, 121.245, -65.25);
+  MetroData.createCity(3, "MyCity", 10000, 121.245, -25.25);
+  MetroData.createCity(4, "NewYork", 100000, 80.245, 25.25);
+  MetroData.createCity(5, "Inuktituk", 100000, 80.245, 68.25);
+  expect(MetroData.getMostNorthern().city).toEqual("Inuktituk");
+});
 
 test("check whichSphere ", () => {
-	let MetroData = new Community;
-	MetroData.createCity(3, "MyCity", 121.245, -25.250, 10000);
-	MetroData.createCity(4, "NewYork", 80.245, 25.250, 100000);
-	expect(MetroData.whichSphere(3)).toEqual("Southern Hemisphere");
-	expect(MetroData.whichSphere(4)).toEqual("Northern Hemisphere")
-})
-
-
-
+  let MetroData = new Community();
+  MetroData.createCity(3, "MyCity", 10000, -121.245, -25.25);
+  MetroData.createCity(4, "NewYork", 100000, 80.245, 25.25);
+  expect(MetroData.whichSphere(3)).toEqual("Southern Hemisphere");
+  expect(MetroData.whichSphere(4)).toEqual("Northern Hemisphere");
+});
--- src/03-objects/Cities/cityfunctions.test.js
+++ src/03-objects/Cities/cityfunctions.test.js
@@ -1,126 +1,109 @@
-import {utils, functions} from './functions.js';
+import { cutils, cfunctions } from "./cityfunctions.js";
 import * as oo from "../accounts.js";
 
-
-
 test("test setup", () => {
-    console.log("Hello from functions.test.js");
+  console.log("Hello from functions.test.js");
 }),
+  //     test('check Add ', () => {
 
+  //         const ol = document.createElement("ol");
+  //         ol.id = "list-cont";
+  //         let listCount = ol.childElementCount;
 
-//     test('check Add ', () => {
+  //         functions.addListItem("star", ol);
+  //         // console.log(listCount,  ol.childElementCount);
+  //         functions.addListItem("cat", ol);
+  //         // console.log(listCount,  ol.childElementCount);
 
-//         const ol = document.createElement("ol");
-//         ol.id = "list-cont";
-//         let listCount = ol.childElementCount;
+  //         expect(ol.childElementCount).toEqual(listCount + 2);
 
-//         functions.addListItem("star", ol);
-//         // console.log(listCount,  ol.childElementCount);
-//         functions.addListItem("cat", ol);
-//         // console.log(listCount,  ol.childElementCount);
+  //     });
 
-//         expect(ol.childElementCount).toEqual(listCount + 2);
+  // test('check Show ', () => {
 
+  //     const ol = document.createElement("ol");
+  //     ol.id = "list-cont";
+  //     let listCount = ol.childElementCount;
 
-//     });
+  //     functions.addListItem("star", ol);
+  //     // console.log(listCount,  ol.childElementCount);
+  //     functions.addListItem("cat", ol);
+  //     // console.log(listCount,  ol.childElementCount);
 
+  //     expect(ol.childElementCount).toEqual(listCount + 2);
+  //     console.log(listCount, ol.textContent);
+  //     functions.showItems(ol, ol);
+  //     expect(ol.textContent).toEqual("starcat")
 
-// test('check Show ', () => {
+  // });
 
-//     const ol = document.createElement("ol");
-//     ol.id = "list-cont";
-//     let listCount = ol.childElementCount;
+  // test('check Delete ', () => {
 
-//     functions.addListItem("star", ol);
-//     // console.log(listCount,  ol.childElementCount);
-//     functions.addListItem("cat", ol);
-//     // console.log(listCount,  ol.childElementCount);
+  //     const ol = document.createElement("ol");
+  //     ol.id = "list-cont";
+  //     let listCount = ol.childElementCount;
 
-//     expect(ol.childElementCount).toEqual(listCount + 2);
-//     console.log(listCount, ol.textContent);
-//     functions.showItems(ol, ol);
-//     expect(ol.textContent).toEqual("starcat")
+  //     functions.addListItem("star", ol);
+  //     // console.log(listCount,  ol.childElementCount);
+  //     functions.addListItem("cat", ol);
+  //     // console.log(listCount,  ol.childElementCount);
 
-// });
+  //     expect(ol.childElementCount).toEqual(listCount + 2);
+  //     console.log(listCount, ol.textContent);
 
+  //     // Delete the last item
+  //     functions.deleteItem(ol);
+  //     expect(ol.textContent).toEqual("star")
 
-// test('check Delete ', () => {
+  // });
 
-//     const ol = document.createElement("ol");
-//     ol.id = "list-cont";
-//     let listCount = ol.childElementCount;
+  // // Exercise 2
 
-//     functions.addListItem("star", ol);
-//     // console.log(listCount,  ol.childElementCount);
-//     functions.addListItem("cat", ol);
-//     // console.log(listCount,  ol.childElementCount);
+  // const parentBody = document.createElement("div");
+  // parentBody.id = "idLeftPanel"
 
-//     expect(ol.childElementCount).toEqual(listCount + 2);
-//     console.log(listCount, ol.textContent);
+  // functions.addCard(parentBody);
+  // functions.addCard(parentBody);
+  // functions.addCard(parentBody);
+  // functions.addCard(parentBody);
 
-//     // Delete the last item 
-//     functions.deleteItem(ol);
-//     expect(ol.textContent).toEqual("star")
+  // test('Testing addCard function ', () => {
 
-// });
+  //     let countArr = functions.addCard(parentBody);
 
+  //     expect(countArr).toEqual(['1', '2', '3', '4', '5']);
 
-// // Exercise 2
+  // });
 
+  // test('Testing delete functionality ', () => {
 
-// const parentBody = document.createElement("div");
-// parentBody.id = "idLeftPanel"
+  //    // console.log("parentBody :", parentBody.children[2].className);
+  //     // deletes the third child card with count #3
+  //     let countArr = functions.delCard(parentBody.children[2], parentBody);
+  //     //  // Adds a new Card  card with count #5 to the end
+  //     let countArr1 = functions.addCard(parentBody);
+  //     expect(countArr1).toEqual(['1', '2', '4', '5'])
+  // });
 
-// functions.addCard(parentBody);
-// functions.addCard(parentBody);
-// functions.addCard(parentBody);
-// functions.addCard(parentBody);
+  // test('Testing addBefore functionality ', () => {
 
-// test('Testing addCard function ', () => {
+  //     console.log("addBefore :", parentBody.children[2].getAttribute("count") );
+  //     //Insert before Card 4
+  //     let countArr = functions. positionCard(parentBody.children[2], "beforebegin");
+  //     // deletes the third child card with count #3
+  //    // let countArr1 = functions.addCard(parentBody);
+  //     expect(countArr).toEqual(['1', '2', '4', '5'])
+  // });
 
-//     let countArr = functions.addCard(parentBody);
+  test("Testing recover Account Name functionality ", () => {
+    let buttonClickedid = "idDP:Savings";
+    expect(cutils.recoverAccName(buttonClickedid)).toEqual("Savings");
+  });
 
-//     expect(countArr).toEqual(['1', '2', '3', '4', '5']);
-
-// });
-
-// test('Testing delete functionality ', () => {
-
-//    // console.log("parentBody :", parentBody.children[2].className);
-//     // deletes the third child card with count #3 
-//     let countArr = functions.delCard(parentBody.children[2], parentBody);
-//     //  // Adds a new Card  card with count #5 to the end
-//     let countArr1 = functions.addCard(parentBody);
-//     expect(countArr1).toEqual(['1', '2', '4', '5'])
-// });
-
-// test('Testing addBefore functionality ', () => {
-
-//     console.log("addBefore :", parentBody.children[2].getAttribute("count") );
-//     //Insert before Card 4
-//     let countArr = functions. positionCard(parentBody.children[2], "beforebegin");
-//     // deletes the third child card with count #3      
-//    // let countArr1 = functions.addCard(parentBody);
-//     expect(countArr).toEqual(['1', '2', '4', '5'])
-// });
-
-test('Testing recover Account Name functionality ', () => {
-	let buttonClickedid = "idDP:Savings"
-	expect(utils.recoverAccName(buttonClickedid)).toEqual("Savings")
+test.only("Testing Recreate Balance ID functionality ", () => {
+  let account = new oo.Accounts();
+  account = { accName: "Checking Account1", bal: 10 };
+  expect(cutils.recreateIDforBalance(account)).toEqual(
+    "idAB:Checking Account1"
+  );
 });
-
-test.only('Testing Recreate Balance ID functionality ', () => {
-	let account = new oo.Accounts;
-			account = { "accName": "Checking Account1", "bal": 10 }
-	expect(utils.recreateIDforBalance(account)).toEqual("idAB:Checking Account1")
-});
-
-
-
-
-
-
-
-
-
-
