cc21cd32e866a6382e8f9892b18158746985bd5d
stackker <sunil.suniljoseph@gmail.com>
2020-02-21 14:14:17 -0700

    Ex220 A  regex search and  File count Stats

---
+++ Python/Competencies/readFile220_test.py
@@ -0,0 +1,14 @@
+# clears the terminal screen 
+print('\033c')
+
+import pytest
+from readFiles220  import countSearchString as regx
+
+file = 'The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+
+def test_helloWorld():
+    assert "Hello World from readFile220.test.py" == "Hello World from readFile220.test.py"
+
+def test_occurance():
+    assert regx("dog", './readFile220_test.py')== {'totalCharacters': 899, 'totalLines': 14, 'totalWords': 138, 'wordOccurs': 3}
+    assert regx("Lorem", './readFile220_test.py')=={'totalCharacters': 899, 'totalLines': 14, 'totalWords': 138, 'wordOccurs': 2}
---
+++ Python/Competencies/readFiles220.py
@@ -0,0 +1,51 @@
+# clears the terminal screen 
+print('\033c')
+
+
+
+
+def countSearchString(searchString, fileNamePath):
+    """ Takes Regex search Inputs """
+    import re 
+    # fileName = open(fileNamePath, 'r')
+    # file_contents = fileName.read()
+    with open(fileNamePath, 'r') as fileName:
+       
+        regex = re.compile(str(searchString))
+        file_contents = fileName.read().split('\n')
+        # print("FileContents: ",file_contents)
+        lineCount = 0
+        wordCount = 0
+        charCount = 0
+        for lines in file_contents:
+            # print("lines: ", lines)
+            lineCount += 1
+            wordList = lines.strip().split()
+            # print("WordList: ", wordList)
+            wordCount += len(wordList)
+            charCount += sum(len(word) for word in wordList)
+
+        count_occurances = 0  #
+        matches = regex.finditer(str(file_contents))
+        for  index, match in enumerate(matches):
+            print(index, match.group(0)) # group references the () in Regex expression . Not the output of the match
+            count_occurances += 1
+        
+        return {'wordOccurs': count_occurances,
+                'totalLines': lineCount,
+                'totalWords': wordCount,
+                'totalCharacters': charCount}
+
+    
+
+
+
+# ----------------------------------------------------
+# if this check is not there it will run the below portion inspite of Calling only the Func above in Test
+
+if __name__ == '__main__':
+    fileNamePath= input('Enter File name + Path: ')
+    searchStr = input("Search String : ")
+
+    print('Count: ',countSearchString(searchStr, fileNamePath))
+
